574b48f3fb32ab39c70f78bb6c6d428524dd8624
zangzang <x2y@live.com>
2022-04-08 10:53:07 +0900

    4 files modified, 2 added

---
+++ src/components/PQGrid.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+defineProps<{ options: object }>();
+onMounted(() => {
+  pq.grid(this.$el, this.options);
+});
+</script>
+
+<template>
+  <div :options="options"></div>
+</template>
+
+<style scoped></style>
